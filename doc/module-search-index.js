731982ea9cfd709eaaee010311244938e6121c11
Angel <ladotecnologico@gmail.com>
2024-03-22 01:51:09 +0100

    Primer cambio

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Animales"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"estructura","l":"Animal"},{"p":"estructura","l":"Insecto"},{"p":"estructura","l":"Main"}];updateSearchResults();
